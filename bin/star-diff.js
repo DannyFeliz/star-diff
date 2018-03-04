58fd2eae1018c913ed95e2cfb7ae9d5964d88e88
Danny Feliz <dannyfeliz08@gmail.com>
2018-03-04 19:33:50 -0400

    Format cli

--- bin/star-diff.js
+++ bin/star-diff.js
@@ -12,19 +12,19 @@ axios.defaults.baseURL = "https://api.github.com/search/repositories";
 axios.defaults.params = { per_page: 1 };
 
 app
-    .version(packageJson.version)
-    .description(packageJson.description)
-    .usage("<repo1> <repo2> [options]")
-    .arguments("<repo1> <repo2>")
-    .option("-s, --stats", "display request Github stats")
-    .action((repo1, repo2) => {
-        getRepoData(repo1, repo2).then(repos => {
-            if (repos) {
-                displayRepoStats(repos.resultRepo1, repos.resultRepo2);
-            }
-        });
-    })
-    .parse(process.argv);
+  .version(packageJson.version)
+  .description(packageJson.description)
+  .usage("<repo1> <repo2> [options]")
+  .arguments("<repo1> <repo2>")
+  .option("-s, --stats", "display request Github stats")
+  .action((repo1, repo2) => {
+    getRepoData(repo1, repo2).then(repos => {
+      if (repos) {
+        displayRepoStats(repos.resultRepo1, repos.resultRepo2);
+      }
+    });
+  })
+  .parse(process.argv);
 
 if (!app.args.length) app.help();
 
@@ -32,7 +32,7 @@ if (!app.args.length) app.help();
  * Print an empty line
  */
 function emptyLine() {
-    log("");
+  log("");
 }
 
 /**
@@ -40,13 +40,17 @@ function emptyLine() {
  * @param {string} repo
  */
 function repoNotFound(repo) {
-    log(colors.red(`Sorry the repository ${repo} was not found`));
+  log(colors.red(`Sorry the repository ${repo} was not found`));
 }
 
 function rateLimitReached() {
-    emptyLine();
-    log(colors.red(`You reached the call limit per minute to the Github search API. ` +
-        `More info here: https://developer.github.com/v3/search/#rate-limit`));
+  emptyLine();
+  log(
+    colors.red(
+      `You reached the call limit per minute to the Github search API. ` +
+      `More info here: https://developer.github.com/v3/search/#rate-limit`
+    )
+  );
 }
 
 // Global error flag to know if any error have been thrown
@@ -57,27 +61,35 @@ let hasError = false;
  * @param {string} repo
  */
 async function fetchRepoData(repo) {
-    emptyLine();
-    log(colors.blue(`Searching ${repo}...`));
+  emptyLine();
+  log(colors.blue(`Searching ${repo}...`));
 
-    let resultRepo = await axios({ params: { q: repo } }).then(data => {
-        if (!data.data.total_count) return;
-        updateHeaderData(data.headers);
-        log(colors.blue(`Found https://github.com/${data.data.items[0].full_name}`));
-        return data.data.items[0];
-    }).catch((error, otro, param) => {
-        let resetTime = moment.unix(error.response.headers["x-ratelimit-reset"]);
-        const agoTime = resetTime.fromNow();
-        const formatedTime = resetTime.format("hh:mm:ss A");
-        rateLimitReached();
-        emptyLine();
-        log(colors.yellow(`You will be able to make requests again ${agoTime} (${formatedTime})`));
-        hasError = true;
+  let resultRepo = await axios({ params: { q: repo } })
+    .then(data => {
+      if (!data.data.total_count) return;
+      updateHeaderData(data.headers);
+      log(
+        colors.blue(`Found https://github.com/${data.data.items[0].full_name}`)
+      );
+      return data.data.items[0];
     })
+    .catch((error, otro, param) => {
+      let resetTime = moment.unix(error.response.headers["x-ratelimit-reset"]);
+      const agoTime = resetTime.fromNow();
+      const formatedTime = resetTime.format("hh:mm:ss A");
+      rateLimitReached();
+      emptyLine();
+      log(
+        colors.yellow(
+          `You will be able to make requests again ${agoTime} (${formatedTime})`
+        )
+      );
+      hasError = true;
+    });
 
-    emptyLine();
+  emptyLine();
 
-    return resultRepo;
+  return resultRepo;
 }
 
 /**
@@ -86,37 +98,39 @@ async function fetchRepoData(repo) {
  * @param {object} repo2
  */
 async function getRepoData(repo1, repo2) {
+  log(
+    colors.yellow(
+      "Hint: If you want to be more especify you can add the full name of the repository. E.g. vuejs/vue"
+    )
+  );
 
-    log(colors.yellow("Hint: If you want to be more especify you can add the full name of the repository. E.g. vuejs/vue"));
+  let resultRepo1 = await fetchRepoData(repo1);
 
-    let resultRepo1 = await fetchRepoData(repo1);
+  if (hasError) return;
+  if (!resultRepo1) return repoNotFound(repo1);
 
-    if (hasError) return;
-    if (!resultRepo1) return repoNotFound(repo1);
+  resultRepo1 = {
+    name: resultRepo1.name,
+    stars: resultRepo1.stargazers_count
+  };
 
-    resultRepo1 = {
-        name: resultRepo1.name,
-        stars: resultRepo1.stargazers_count
-    };
+  let resultRepo2 = await fetchRepoData(repo2);
 
-    let resultRepo2 = await fetchRepoData(repo2);
+  if (hasError) return;
+  if (!resultRepo2) return repoNotFound(repo2);
 
-    if (hasError) return;
-    if (!resultRepo2) return repoNotFound(repo2);
+  resultRepo2 = {
+    name: resultRepo2.name,
+    stars: resultRepo2.stargazers_count
+  };
 
-    resultRepo2 = {
-        name: resultRepo2.name,
-        stars: resultRepo2.stargazers_count
-    };
-
-    return { resultRepo1, resultRepo2 };
+  return { resultRepo1, resultRepo2 };
 }
 
-
 const headerData = {
-    limit: 0,
-    remaining: 0,
-    resetTime: 0
+  limit: 0,
+  remaining: 0,
+  resetTime: 0
 };
 
 /**
@@ -124,9 +138,9 @@ const headerData = {
  * @param {object} headers
  */
 function updateHeaderData(headers) {
-    headerData.limit = headers["x-ratelimit-limit"];
-    headerData.remaining = headers["x-ratelimit-remaining"];
-    headerData.resetTime = headers["x-ratelimit-reset"];
+  headerData.limit = headers["x-ratelimit-limit"];
+  headerData.remaining = headers["x-ratelimit-remaining"];
+  headerData.resetTime = headers["x-ratelimit-reset"];
 }
 
 /**
@@ -136,64 +150,65 @@ function updateHeaderData(headers) {
  * @param {object} repo2
  */
 function displayRepoStats(repo1, repo2) {
+  const difference = Math.abs(repo1.stars - repo2.stars);
+  let winnerMessage = "";
+  if (!difference) {
+    winnerMessage = "Both repositories have the same number of stars";
+  } else {
+    const winner = repo1.stars > repo2.stars ? repo1.name : repo2.name;
+    winnerMessage = `${winner} wins with ${difference.toLocaleString()}`;
+  }
 
-    const difference = Math.abs(repo1.stars - repo2.stars);
-    let winnerMessage = "";
-    if (!difference) {
-        winnerMessage = "Both repositories have the same number of stars";
-    } else {
-        const winner = repo1.stars > repo2.stars ? repo1.name : repo2.name;
-        winnerMessage = `${winner} wins with ${difference.toLocaleString()}`
-    }
+  let winnerColor = "";
+  if (repo1.stars > repo2.stars) {
+    winnerColor = "blue";
+  } else if (repo1.stars < repo2.stars) {
+    winnerColor = "green";
+  } else {
+    winnerColor = "yellow";
+  }
 
-    let winnerColor = "";
-    if (repo1.stars > repo2.stars) {
-        winnerColor = "blue";
-    } else if (repo1.stars < repo2.stars) {
-        winnerColor = "green";
-    } else {
-        winnerColor = "yellow";
-    }
+  const repoTable = new cliTable({
+    head: ["PROJECT", "STARS"]
+  });
 
-    const repoTable = new cliTable({
-        head: ["PROJECT", "STARS"]
-    });
+  repoTable.push(
+    [colors.blue(repo1.name), colors.blue(repo1.stars.toLocaleString())],
+    [colors.green(repo2.name), colors.green(repo2.stars.toLocaleString())],
+    [
+      {
+        colSpan: 2,
+        content: colors[winnerColor](winnerMessage)
+      }
+    ]
+  );
 
-    repoTable.push(
-        [colors.blue(repo1.name), colors.blue(repo1.stars.toLocaleString())],
-        [colors.green(repo2.name), colors.green(repo2.stars.toLocaleString())],
-        [{
-            colSpan: 2,
-            content: colors[winnerColor](winnerMessage)
-        }]
-    );
+  log(repoTable.toString());
 
-    log(repoTable.toString());
-
-    if (app.stats) {
-        displayGithubStats();
-    }
+  if (app.stats) {
+    displayGithubStats();
+  }
 }
 
 /**
  * Display the github stats when the -s flag is passed
  */
 function displayGithubStats() {
-    const originalResetDate = moment.unix(headerData.resetTime);
-    const resetTime = {
-        originalResetDate,
-        agoTime: originalResetDate.fromNow(),
-        formatedTime: originalResetDate.format("hh:mm:ss A")
-    };
-    const table = new cliTable({
-        head: ["LIMIT", "REMAINING REQUESTS", "RESET TIME"]
-    });
+  const originalResetDate = moment.unix(headerData.resetTime);
+  const resetTime = {
+    originalResetDate,
+    agoTime: originalResetDate.fromNow(),
+    formatedTime: originalResetDate.format("hh:mm:ss A")
+  };
+  const table = new cliTable({
+    head: ["LIMIT", "REMAINING REQUESTS", "RESET TIME"]
+  });
 
-    table.push([
-        colors.yellow(headerData.limit),
-        colors.yellow(headerData.remaining),
-        colors.yellow(`${resetTime.agoTime} (${resetTime.formatedTime})`),
-    ]);
+  table.push([
+    colors.yellow(headerData.limit),
+    colors.yellow(headerData.remaining),
+    colors.yellow(`${resetTime.agoTime} (${resetTime.formatedTime})`)
+  ]);
 
-    log(table.toString());
+  log(table.toString());
 }
